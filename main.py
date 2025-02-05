c19dba8282320c1c60ea8ee2110e6b62e5eeb3b0
watkin98 <wvatkin1@gmail.com>
2025-02-04 18:16:43 -0700

    added a report generating function

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    '''
-    print(file_contents)
-    return 0
-    '''
-    #get_num_of_words(file_contents)
+    word_count = get_num_of_words(file_contents)
     number_of_chars_in_string = get_num_of_each_char(file_contents)
-    print(number_of_chars_in_string)
+
+    report = generate_report("books/frankenstein.txt", word_count, number_of_chars_in_string)
+    print(report)
 
 def get_num_of_words(words):
     count = 0
@@ -19,7 +17,7 @@ def get_num_of_words(words):
     for word in words_in_list:
         count += 1
 
-    print(count)
+    return count
 
 def get_num_of_each_char(words):
     num_of_each_char = {}
@@ -35,5 +33,24 @@ def get_num_of_each_char(words):
             num_of_each_char[lowered_character] += 1
         
     return num_of_each_char
+
+def generate_report(file_path, word_count, character_dictionary):
+    report = ""
+    
+    report += f"--- Begin report of {file_path} ---\n"
+    report += f"{word_count} words found in the document\n"
+
+    list_of_alphabet_counts = []
+
+    for entry in character_dictionary:
+        if entry.isalpha():
+            list_of_alphabet_counts.append([entry, character_dictionary[entry]])
+
+    for char in list_of_alphabet_counts:
+        report += f"The '{char[0]}' character was found {char[1]} times\n"
+
+    report += "--- End report ---"
+
+    return report
 
 main()
