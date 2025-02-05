7e94ca9a422b11b59291e1c4fff1d7604e8c01b3
watkin98 <wvatkin1@gmail.com>
2025-02-04 17:54:46 -0700

    added function to count number of characters in a string, returns a dictionary

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def main():
     print(file_contents)
     return 0
     '''
-    num_of_words(file_contents)
+    #get_num_of_words(file_contents)
+    number_of_chars_in_string = get_num_of_each_char(file_contents)
+    print(number_of_chars_in_string)
 
-def num_of_words(words):
+def get_num_of_words(words):
     count = 0
     words_in_list = words.split()
 
@@ -18,5 +20,20 @@ def num_of_words(words):
         count += 1
 
     print(count)
+
+def get_num_of_each_char(words):
+    num_of_each_char = {}
+
+    characters = list(words)
+
+    for character in characters:
+        lowered_character = character.lower()
+        
+        if lowered_character not in num_of_each_char:
+            num_of_each_char[lowered_character] = 1
+        else:
+            num_of_each_char[lowered_character] += 1
+        
+    return num_of_each_char
 
 main()
